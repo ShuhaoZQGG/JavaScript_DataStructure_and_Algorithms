de403746f8fcbbb1bfe05ef906d1d66f2646b5e7
unknown <shuhao.zhang@mail.mcgill.ca>
2021-09-03 17:45:07 -0400

    add new features

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -120,6 +120,108 @@ class BST{
     }
     this.root = removeNode(this.root, data);
   }
+
+  findMinHeight(node = this.root){
+    if (node == null){
+      return -1;
+    }
+    let left = this.findMinHeight(node.left);
+    let right = this.findMinHeight(node.right);
+    if (left < right){
+      return left + 1;
+    }
+    else{
+      return right + 1
+    }
+  }
+
+  findMaxHeight(node = this.root){
+    if (node == null){
+      return -1;
+    }
+    let left = this.findMaxHeight(node.left);
+    let right = this.findMaxHeight(node.right);
+    if (left > right){
+      return left + 1;
+    }
+    else{
+      return right + 1
+    }
+  }
+
+  isBalanced(){
+    return (this.findMaxHeight() - this.findMinHeight() <= 1);
+  } 
+
+  inOrder(){
+    if (this.root == null){
+      return null;
+    }
+    else{
+      let result = new Array();
+      function traverseInOrder(node){
+        node.left && traverseInOrder(node.left);
+        result.push(node.data);
+        node.right && traverseInOrder(node.right);
+      }
+      traverseInOrder(this.root);
+      return result;
+    }
+  }
+
+  preOrder(){
+    if (this.root == null){
+      return null;
+    }
+    else{
+      let result = new Array();
+      function traversePreOrder(node){
+        result.push(node.data);
+        node.left && traversePreOrder(node.left);
+        node.right && traversePreOrder(node.right);
+      }
+      traversePreOrder(this.root);
+      return result;
+    }
+  }
+
+  postOrder(){
+    if (this.root == null){
+      return null;
+    }
+    else{
+      let result = new Array();
+      function traversePostOrder(node){
+        node.left && traversePostOrder(node.left);
+        node.right && traversePostOrder(node.right);
+        result.push(node.data);
+      }
+      traversePostOrder(this.root);
+      return result;
+    }
+  }
+
+  levelOrder(){
+    let result = new Array();
+    let Queue = new Array();
+    if (this.root != null){
+      Queue.push(this.root);
+      while(Queue.length > 0){
+        let node = Queue.shift();
+        result.push(node.data);
+        if(node.left != null){
+          Queue.push(node.left);
+        }
+        if(node.right != null){
+          Queue.push(node.right);
+        }
+      }
+      return result;
+    }
+    else{
+      return null;
+    }
+  }
 }
 
 const bst = new BST();
@@ -127,12 +229,21 @@ const bst = new BST();
 bst.add(5);
 bst.add(6);
 bst.add(7);
+bst.add(2)
 bst.add(1);
 bst.add(3);
 bst.add(5.5);
 console.log(JSON.stringify(bst));
+console.log(bst.findMinHeight())
 bst.remove(5);
 console.log(bst.findMin());
 console.log(bst.findMax());
 console.log(bst.find(5.5));
 console.log(JSON.stringify(bst));
+console.log(bst.findMinHeight());
+console.log(bst.findMaxHeight());
+console.log(bst.isBalanced());
+console.log(bst.inOrder());
+console.log(bst.preOrder());
+console.log(bst.postOrder());
+console.log(bst.levelOrder());
